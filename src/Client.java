43310854efaa5aa9b83d02c601950cd6c910f00e
Arvid Utas <arvidu@hotmail.com>
2024-11-15 11:39:08 +0100

    First commit. Add main classes and .properties file

---
+++ src/Client.java
@@ -0,0 +1,9 @@
+public class Client {
+    public Client() {
+
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+    }
+}
---
+++ src/ServerListener.java
@@ -0,0 +1,9 @@
+public class ServerListener {
+    public ServerListener() {
+
+    }
+
+    public static void main(String[] args) {
+        ServerListener sl = new ServerListener();
+    }
+}
