ce1d9d1bfea0f1e0c1758cd56643665365ede0b3
Arvid Utas <arvidu@hotmail.com>
2024-11-18 12:14:48 +0100

    More work on Player constructor, added sendToClient method

--- src/ServerSide/Player.java
+++ src/ServerSide/Player.java
@@ -1,14 +1,32 @@
 package ServerSide;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Player {
     private Socket socket;
     private char playerNum;
     private Player opponent;
+    private BufferedReader in;
+    private PrintWriter out;
 
     public Player(Socket socket, char playerNum) {
         this.socket = socket;
         this.playerNum = playerNum;
+        try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void sendToClient(String message){
+        out.println(message);
+    }
+
+
 }
