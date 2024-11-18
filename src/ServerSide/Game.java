56dda3853596f559957797c599c24048e4b62fb9
axelgentile <gentileaxel466@gmail.com>
2024-11-18 23:45:36 +0100

    spellogik + metoder i question

--- src/ServerSide/Game.java
+++ src/ServerSide/Game.java
@@ -8,6 +8,9 @@ public class Game extends Thread {
     Category category = new Category();
     private int numQuestion; //TODO: dessa ska styras av properties filen sen
     private int numRounds; //TODO: dessa ska styras av properties filen sen
+    private int p1Score = 0;
+    private int p2Score = 0;
+
 
 
     public Game(Player player1, Player player2) {
@@ -15,6 +18,11 @@ public class Game extends Thread {
         this.player2 = player2;
         this.player1.setOpponent(player2);
         this.player2.setOpponent(player1);
+    }
+
+    public void sendToBothClients(String message) {
+        player1.sendToClient(message);
+        player2.sendToClient(message);
     }
 
     public void run() {
@@ -27,14 +35,47 @@ public class Game extends Thread {
         sendToBothClients(questions.getFirst().get(4));
         sendToBothClients(questions.getFirst().get(5));
 
+
+
+        if (questions.isEmpty()) {
+            throw new IllegalStateException("Inga frågor tillgängliga för spelet.");
+        }
+
+
+
         for (int round = 1; round <= numRounds; round++) {
             player1.sendToClient("Runda " + round);
             player2.sendToClient("Runda " + round);
+            Question currentQuestion = null;
+
+
+            for (int q = 0; q < numRounds; q++) {
+                sendToBothClients(currentQuestion.toString());
+
+                String player1Answer = player1.receieveFromClient();
+                String player2Answer = player2.receieveFromClient();
+
+                if (currentQuestion.isCorrect(player1Answer)) p1Score++;
+                if (currentQuestion.isCorrect(player2Answer)) p2Score++;
+            }
+
+
+            player1.sendToClient("Slutpoäng: " + p1Score);
+            player2.sendToClient("Slutpoäng: " + p2Score);
+
+            if (p1Score > p2Score) {
+                player1.sendToClient("Du vann");
+                player2.sendToClient("Du förlorade");
+            } else if (p1Score < p2Score) {
+                player1.sendToClient("Du förlorade");
+                player2.sendToClient("Du vann");
+            } else {
+                player1.sendToClient("Oavgjort");
+                player2.sendToClient("Oavgjort");
+            }
+
+
         }
     }
-
-    public void sendToBothClients (String message){
-        player1.sendToClient(message);
-        player2.sendToClient(message);
-    }
 }
+
